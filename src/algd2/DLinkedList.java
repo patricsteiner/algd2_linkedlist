c54df2b35cb91de64fed545d05497e8ddd253aa1
Jan Hitz <jan.hitz@students.fhnw.ch>
2016-10-16 01:06:42 +0200

    added methods

--- src/algd2/DLinkedList.java
+++ src/algd2/DLinkedList.java
@@ -206,19 +206,26 @@ public class DLinkedList<E> extends AbstractList<E> implements IList<E> {
 
 	@Override
 	public E get(ListItem item) {
-		// TODO Auto-generated method stub
+		if (checkMembership(item)) {
+			return (E) item.data;
+		}
 		return null;
 	}
 
 	@Override
 	public void set(ListItem item, E data) {
-		// TODO Auto-generated method stub
-		
+		if (checkMembership(item)) {
+			item.data = data;
+		}
 	}
 
 	@Override
 	public E remove(ListItem item) {
-		// TODO Auto-generated method stub
+		if (checkMembership(item)) {
+			E ret = (E) item.data;
+			delete(item, true);
+			return ret;
+		}
 		return null;
 	}
 
@@ -238,39 +245,64 @@ public class DLinkedList<E> extends AbstractList<E> implements IList<E> {
 
 	@Override
 	public ListItem addAfter(ListItem item, E data) {
-		ListItem<E> newitem = new ListItem<>(data);
-		linkInAfter(item, newitem);
-		return newitem;
+		if (checkMembership(item)) {
+			ListItem<E> newitem = new ListItem<>(data);
+			linkInAfter(item, newitem);
+			return newitem;
+		}
+		return null;
 	}
 
 	@Override
 	public ListItem addBefore(ListItem item, E data) {
-		// TODO Auto-generated method stub
+		if (checkMembership(item)) {
+			ListItem<E> newitem = new ListItem<>(data);
+			linkInAfter(item.prev, newitem);
+			return null;
+		}
 		return null;
 	}
 
 	@Override
 	public void moveToHead(ListItem item) {
-		// TODO Auto-generated method stub
-		
+		if (checkMembership(item)) {
+			unlink(item);
+			linkInFront(item);
+		}
 	}
 
 	@Override
 	public void moveToTail(ListItem item) {
-		// TODO Auto-generated method stub
-		
+		if (checkMembership(item)) {
+			unlink(item);
+			linkInBack(item);
+		}
 	}
 
 	@Override
 	public void rotate(ListItem item) {
-		// TODO Auto-generated method stub
-		
+		if (checkMembership(item) && item != head) {
+			tail = item.prev;
+			item.prev.next = null;
+			head = item;
+			item.prev = null;
+		}
 	}
 
 	@Override
 	public void swap(ListItem item1, ListItem item2) {
-		// TODO Auto-generated method stub
-		
+		if (checkMembership(item1) && checkMembership(item2) && !item1.equals(item2)) {
+			if (item1 == head) head = item2;
+			else if (item2 == head) head = item1;
+			if (item1 == tail) tail = item2;
+			else if (item2 == tail) tail = item1;
+			ListItem tmpPrev = item1.prev;
+			ListItem tmpNext = item1.next;
+			item1.prev = item2.prev;
+			item1.next = item2.next;
+			item2.prev = tmpPrev;
+			item2.next = tmpNext;
+		}
 	}
 
 	@Override
